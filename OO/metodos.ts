9a065c5f6fc8bfd167870c51c0275a6ce6a2bb56
Paulo Santos <pauloed_@outlook.com>
2022-04-21 21:14:34 -0300

    Tipos de métodos e tipos de modificadores de acesso

--- OO/metodos.ts
+++ OO/metodos.ts
@@ -8,8 +8,18 @@ class Pessoa {
     public falar(nome:string) { //Assim como os atributos podemos adicionar a palavra reservada public também aos métodos
         return `Oi meu nome é ${nome}`
     }
+
+    public idadePessoa(): number {
+        return this.idade
+    }
+
+    private rendaPessoa() { //Podemos definir métodos privados com a palavra reservada private
+        return "3000"
+    }
 }
 
 const novaPessoa = new Pessoa("Maria", 25, "feminino")
 
 console.log(novaPessoa.falar(novaPessoa.nome))
+
+console.log(novaPessoa.idadePessoa())
--- OO/modificadoresDeAcesso.ts
+++ OO/modificadoresDeAcesso.ts
@@ -4,9 +4,27 @@ class Visibilidade {
 
     public nome: string //Por padrão todos os atributos são públicos, mesmo sem usar a palavra reservada public
     public idade: number
+    private cpf: string //O private torna o atributo privado, impedindo assim que seja acessado ou diretamente
 
-    constructor(nome:string, idade: number) {
+    constructor(nome:string, idade: number, cpf: string) {
         this.nome = nome
         this.idade = idade
+        this.cpf = cpf
+    }
+
+    public falaNome(): string {
+        return this.nome
+    }
+
+    private validaIdade(data:number) { //Métodos privados servem para serem usados dentro da própria classe
+        if(data > 18) return true
+    }
+
+    public permissao(idade: number) {
+        return this.validaIdade(idade)
     }
 }
+
+const teste1 = new Visibilidade("pessoa1", 24, "001.345.290-55")
+
+console.log(`Posso beber? ${teste1.permissao(teste1.idade)}`)

fbf192d06b35495af01add30bbbeee3f9b4203f9
Paulo Santos <pauloed_@outlook.com>
2022-05-22 19:38:19 -0300

    decorators

---
+++ decorators/decorators.ts
@@ -0,0 +1,10 @@
+// É um tipo de função que pode ser utilizado para se associar a um elemento e realizar uma ação
+/*
+@novoDecorator // Através do rótudo define-se o decorator
+class ExemploClasse{
+
+}
+
+function novoDecorator(classe: Function) {
+    return console.log(classe)
+}*/
